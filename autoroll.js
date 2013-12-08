e1e842e62eb4665f7877a1476ec7277f233b8a4e
zx81 <hashtagzx81@gmail.com>
2013-12-09 11:52:42 +1300

    Update autoroll.js

--- autoroll.js
+++ autoroll.js
@@ -14,7 +14,8 @@ autoroll.init = function(){
   $("#autoroll-start").on("click", function(){
     if($('#autoroll-start').text() == "Start"){
         
-        var base = Number($("#autoroll-base").val()).toFixed(8);
+        var base = Number($("#autoroll-base").val());
+        var odds = Number($("#autoroll-odds").val()).toFixed(4);
         var onWinReturnToBase = $('#onWinReturnToBase').prop('checked');
         var onWinMultiplier = Number($("#onWinMultiplier").val());
         var onLoseReturnToBase = $('#onLoseReturnToBase').prop('checked');
@@ -29,6 +30,7 @@ autoroll.init = function(){
         // validation for input fields
         if(
             isNaN(base) || 
+            isNaN(odds) || 
             isNaN(max_rolls) || 
             isNaN(onWinMultiplier) || 
             isNaN(onLoseMultiplier)
@@ -37,7 +39,8 @@ autoroll.init = function(){
 
         $('#autoroll-start').text("Stop");
         
-        autoroll.base = base;
+        autoroll.base = base.toFixed(8);
+        autoroll.odds = odds.toFixed(4);
         autoroll.target = isLow ? 0 : 1;
         autoroll.onWin = onWinReturnToBase ? true : onWinMultiplier;
         autoroll.onLose = onLoseReturnToBase ? true : onLoseMultiplier;
@@ -76,7 +79,50 @@ autoroll.init = function(){
 }
 
 autoroll.start = function(){
+
 console.log(this);
+
+    // ripped from glib_dice_roll
+    if((autoroll.bet<mDiceBetMin) || (bet>mDiceBetMax)){
+        TheGamingLib.UI.MessageBox.Alert(
+            'Invalid bet range!',
+            ('Minimum bet: '
+                +mDiceBetMin.toFixed(8)
+                +'<br/> Maximum bet: '
+                +mDiceBetMax.toFixed(8)
+            ),
+            {topOffset:-70}
+        );
+        return false;
+    }
+    
+    mDiceRolling=true;
+
+    var url = mDiceApiUrl
+        +'&action=roll&stat=on&seed='+mDiceFairCS
+        +'&cur='+mGLibUserCurUse
+        +'&roll='+autoroll.odds
+        +'&target='+autoroll.target
+        +'&bet='+autoroll.bet
+
+/* using a customised ajax request response
+    TheGamingLib.Ajax.getDataReturnText(
+        (mDiceApiUrl+'&action=roll&stat=on&seed='+mDiceFairCS+'&cur='+mGLibUserCurUse+'&roll='+tp+'&target='+rol+'&bet='+bet), 
+        glib_dice_roll_data
+    );
+*/
+
+    $.ajax({url:url}).done(autoroll.response);
+
+}
+
+autoroll.response = function(jdt){
+
+    console.log(jdt);
+
+}
+
+
 }
 
 autoroll.stop = function(){
