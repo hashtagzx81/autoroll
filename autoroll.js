b3f7e6e6ac001986b3925f05c33411f8842684b3
zx81 <hashtagzx81@gmail.com>
2013-12-08 23:25:43 +1300

    Update autoroll.js

--- autoroll.js
+++ autoroll.js
@@ -4,6 +4,7 @@ var autoroll = {};
 var autorollInit = function(){
 
   autoroll.enabled_input_backgroundColor = $('#onLoseMultiplier').css('background-color');
+  autoroll.enabled_label_color = $('#onLoseMultiplierLabel').css('color');
   
   $("#autoroll-show").on("click", function(){
     if($('#autoroll-form').is(':visible')){
@@ -17,9 +18,11 @@ var autorollInit = function(){
     if($("#onWinMultiplier").prop('disabled')){
         $("#onWinMultiplier").prop('disabled', false);    
         $('#onWinMultiplier').css('background-color', autoroll.enabled_input_backgroundColor);
+        $('#onLoseMultiplierLabel').css('color', 'autoroll.enabled_label_color');
 } else {
         $("#onWinMultiplier").prop('disabled', true);
         $('#onWinMultiplier').css('background-color', 'grey');
+        $('#onWinMultiplierLabel').css('color', 'grey')
     }
 
   });
@@ -28,13 +31,17 @@ var autorollInit = function(){
     if($("#onLoseMultiplier").prop('disabled')){
         $("#onLoseMultiplier").prop('disabled', false);
         $('#onLoseMultiplier').css('background-color', autoroll.enabled_input_backgroundColor);
+        $('#onLoseMultiplierLabel').css('color', 'autoroll.enabled_label_color');
     } else {
         $("#onLoseMultiplier").prop('disabled', true);    
         $('#onLoseMultiplier').css('background-color', 'grey');
+        $('#onLoseMultiplierLabel').css('color', 'grey')
     }    
   });
 
 
+  // disable onLose checkbox
+  $('#onLoseReturnToBase').click();
   console.log('autoroll initialised');
 
 }
