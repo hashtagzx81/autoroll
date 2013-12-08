cc67f0468644bc392b836024c003792e6ec18695
zx81 <hashtagzx81@gmail.com>
2013-12-08 20:14:44 +1300

    Update autoroll.js

--- autoroll.js
+++ autoroll.js
@@ -1,6 +1,12 @@
 // make the form do what it should
 (function(){
 
-console.log("autoroll script loaded");
+  $("#autoroll-show").on("click", function(){
+    if($('#autoroll-form').is(':visible')){
+        $('#autoroll-form').hide();        
+    } else {
+        $('#autoroll-form').show()
+    }
+  });
 
 })();
